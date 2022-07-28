3704d53dc7146e6692a77286d58f51333c4adb5c
林林 <linlinabcb.icoc@foxmail.com>
2022-07-28 16:57:02 +0800

    Update travelling.js

--- assets/travelling.js
+++ assets/travelling.js
@@ -598,6 +598,7 @@ function travelling() {
   url[593] = 'https://champhoon.xyz';
   url[594] = 'https://moechun.fun';
   url[595] = 'https://www.cnortles.top';
+  url[596] = 'https://sxrekord.com';
 
   url[url.length] = "https://github.com/travellings-link/travellings";
 
