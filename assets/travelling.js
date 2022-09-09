5036a7d2ee5a6f89ea3f6fe9d78364b8c24b59f6
Tang <1024830255@qq.com>
2022-09-09 14:01:17 +0800

    Update travelling.js

--- assets/travelling.js
+++ assets/travelling.js
@@ -186,7 +186,7 @@ function travelling() {
   urls[179] = "senorui.top";
   urls[180] = empty;
   urls[181] = "blog.lihaojin.cn";
-  urls[182] = "www.saroin.com";
+  urls[182] = empty;
   urls[183] = "zhenwu99.gitee.io";
   urls[184] = empty;
   urls[185] = empty;
@@ -491,7 +491,7 @@ function travelling() {
   urls[484] = "www.mabby.cn";
   urls[485] = "aibofan.com";
   urls[486] = "fu1fan.cn";
-  urls[487] = "jerrydodo.com";
+  urls[487] = empty;
   urls[488] = "zhengxi.io";
   urls[489] = "v2hot.pipecraft.net";
   urls[490] = "dvel.me";
