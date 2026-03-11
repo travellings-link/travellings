eee8d43656f6818ca4938ea6d558dcef3a55c876
Allenyou <i@allenyou.wang>
2026-03-11 11:58:32 +0800

    feat: 增加云计算资源赞助商

---
+++ .vitepress/theme/components/ExtraFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="layout-bottom">
+        <span class="copyright">云计算资源赞助商：</span>
+        <a href="https://chuqiyun.com/"><img width="200" src="/assets/chuqiyun-sponsor.png" alt="chuqiyun-sponsor"></a>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.layout-bottom {
+    margin-bottom: 2rem;
+    margin-top: -1rem;
+    z-index: 50;
+    padding: 0 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.copyright {
+    line-height: 24px;
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--vp-c-text-2);
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,19 +1,21 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import Theme from "vitepress/theme";
+import "./style.css";
 // import Layout from "./Layout.vue" disable for a while by author component
-import Authors from "./components/Author.vue"
+import Authors from "./components/Author.vue";
+import ExtraFooter from "./components/ExtraFooter.vue";
 
 export default {
-  extends: Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      "doc-footer-before": () => h(Authors),
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}
+	extends: Theme,
+	Layout: () => {
+		return h(Theme.Layout, null, {
+			// https://vitepress.dev/guide/extending-default-theme#layout-slots
+			"doc-footer-before": () => h(Authors),
+			"layout-bottom": () => h(ExtraFooter),
+		});
+	},
+	enhanceApp({ app, router, siteData }) {
+		// ...
+	},
+};
